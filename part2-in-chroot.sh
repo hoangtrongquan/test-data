11df33fa22a7cd1e9b4521ea5d6e9046b05d885c
hoangtrongquan <48304320+hoangtrongquan@users.noreply.github.com>
2025-07-29 11:02:08 +0700

    update

--- part2-in-chroot.sh
+++ part2-in-chroot.sh
@@ -1,120 +1,121 @@
 #!/bin/bash
 set -e
  
-echo "[1] Cấu hình múi giờ, ngôn ngữ..."
+echo "[1] Đặt múi giờ, locale, hostname..."
 ln -sf /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
 hwclock --systohc
-sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+ 
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "hoang-pc" > /etc/hostname
  
-echo "arch" > /etc/hostname
-cat <<EOF > /etc/hosts
-127.0.0.1 localhost
-::1       localhost
-127.0.1.1 arch.localdomain arch
-EOF
+echo "[2] Cài user..."
+useradd -m -G wheel,audio,video,network hoang
+echo "Đặt mật khẩu cho user hoang:"
+passwd hoang
+echo "Đặt mật khẩu root:"
+passwd
  
-echo "[2] Cài GRUB + Theme Vimix"
+echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/wheel
+ 
+echo "[3] Cài GRUB + theme đẹp..."
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
-pacman -S --noconfirm git
-git clone --depth=1 https://github.com/vinceliuice/grub2-themes /opt/grub2-themes
-bash /opt/grub2-themes/install.sh -b -t vimix -s 1080p
+pacman -S --noconfirm grub-theme-vimix
+mkdir -p /boot/grub/themes
+cp -r /usr/share/grub/themes/Vimix /boot/grub/themes/
+echo 'GRUB_THEME="/boot/grub/themes/Vimix/theme.txt"' >> /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
  
-echo "[3] Đặt mật khẩu root..."
-echo "root:hoangquan" | chpasswd
+echo "[4] Cài đồ họa: Hyprland, drivers, Wayland tools..."
+pacman -S --noconfirm hyprland xdg-desktop-portal-hyprland \
+mesa vulkan-intel libva-intel-driver libva-utils \
+nvidia nvidia-utils nvidia-settings nvidia-dkms \
+waybar rofi alacritty kitty dolphin neofetch \
+ttf-jetbrains-mono ttf-firacode-nerd ttf-font-awesome \
+sddm qt5-graphicaleffects qt5-quickcontrols2 qt5-svg qt5-quickcontrols \
+qt6-wayland qt6-svg qt6-quickcontrols2 qt6-graphicaleffects \
+zsh zsh-autosuggestions zsh-syntax-highlighting \
+pipewire wireplumber pamixer pavucontrol \
+grim slurp wl-clipboard brightnessctl \
+network-manager-applet blueman
  
-echo "[4] Bật dịch vụ khởi động..."
+echo "[5] Cài theme SDDM..."
+git clone --depth=1 https://github.com/vinceliuice/SDDM-Catppuccin.git /tmp/sddm-theme
+/tmp/sddm-theme/install.sh --theme mocha
+mkdir -p /etc/sddm.conf.d
+echo -e "[Theme]\nCurrent=catppuccin-mocha" > /etc/sddm.conf.d/theme.conf
+ 
+echo "[6] Bật dịch vụ..."
 systemctl enable NetworkManager
 systemctl enable sddm
  
-echo "[5] Cài Hyperland, PipeWire, font và app cần thiết..."
-pacman -S --noconfirm --needed \
-xorg xdg-desktop-portal-hyprland hyprland \
-mesa libva libvdpau \
-pipewire wireplumber pamixer \
-sddm qt5-wayland qt6-wayland \
-kitty waybar rofi thunar \
-neovim zsh wget curl unzip \
-ttf-font-awesome ttf-jetbrains-mono noto-fonts noto-fonts-cjk noto-fonts-emoji \
-firefox grim slurp wl-clipboard brightnessctl
+echo "[7] Auto login SDDM vào user hoang và Hyprland..."
+echo -e "[Autologin]\nUser=hoang\nSession=hyprland" > /etc/sddm.conf.d/autologin.conf
  
-echo "[6] Tạo user hoangquan..."
-useradd -mG wheel,input,audio,video -s /bin/zsh hoangquan
-echo "hoangquan:123456" | chpasswd
-echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
+echo "[8] Cài Powerlevel10k..."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/hoang/.p10k
+echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> /home/hoang/.zshrc
+echo 'source ~/.p10k/powerlevel10k.zsh-theme' >> /home/hoang/.zshrc
+echo 'source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh' >> /home/hoang/.zshrc
+echo 'source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> /home/hoang/.zshrc
+chsh -s /bin/zsh hoang
+chown -R hoang:hoang /home/hoang
  
-echo "[7] Thêm theme SDDM (Catppuccin)..."
-git clone --depth=1 https://github.com/catppuccin/sddm.git /usr/share/sddm/themes/catppuccin
-mkdir -p /etc/sddm.conf.d
-echo -e "[Theme]\nCurrent=catppuccin" > /etc/sddm.conf.d/theme.conf
+echo "[9] Tạo config Hyprland..."
+mkdir -p /home/hoang/.config/hypr
+cat <<EOF > /home/hoang/.config/hypr/hyprland.conf
+exec-once = waybar & blueman-applet & nm-applet & pavucontrol & dolphin & alacritty
  
-echo "[8] Powerlevel10k ZSH theme cho hoangquan..."
-sudo -u hoangquan bash <<'EOF'
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.p10k
-echo 'source ~/.p10k/powerlevel10k.zsh-theme' >> ~/.zshrc
-EOF
- 
-echo "[9] Tạo config Hyprland mặc định với blur + shadow + volume key..."
-sudo -u hoangquan bash <<'EOF'
-mkdir -p ~/.config/hypr
-cat > ~/.config/hypr/hyprland.conf <<CONFIG
-exec-once = waybar &
-exec-once = nm-applet &
- 
-monitor = ,preferred,auto,1
+monitor=,preferred,auto,1
  
 input {
-  kb_layout = us
-  follow_mouse = 1
-  touchpad {
-    natural_scroll = yes
-  }
+    kb_layout = us
+    follow_mouse = 1
 }
  
 general {
-  gaps_in = 5
-  gaps_out = 10
-  border_size = 2
-  col.active_border = rgba(33ccffee)
-  col.inactive_border = rgba(595959aa)
+    gaps_in = 5
+    gaps_out = 10
+    border_size = 2
+    col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
+    col.inactive_border = rgba(595959aa)
+    layout = dwindle
 }
  
 decoration {
-  rounding = 10
-  blur {
-    enabled = true
-    size = 8
-    passes = 3
-    new_optimizations = on
-  }
-  drop_shadow = yes
-  shadow_range = 20
-  shadow_render_power = 3
-  col.shadow = rgba(000000aa)
+    rounding = 10
+    blur {
+        enabled = true
+        size = 5
+        passes = 3
+    }
+    drop_shadow = true
+    shadow_range = 4
+    shadow_render_power = 3
+    col.shadow = rgba(00000044)
 }
  
 animations {
-  enabled = yes
-  bezier = ease, 0.25, 0.1, 0.25, 1
-  animation = windows, 1, 3, ease, slide
-  animation = fade, 1, 3, ease
+    enabled = true
+    bezier = myBezier, 0.05, 0.9, 0.1, 1.05
+    animation = windows, 1, 7, myBezier
+    animation = fade, 1, 7, default
+    animation = workspaces, 1, 6, default
 }
  
-bind = SUPER, RETURN, exec, kitty
-bind = SUPER, D, exec, rofi -show drun
-bind = SUPER, Q, killactive,
-bind = SUPER, E, exec, thunar
-bind = SUPER, V, togglefloating,
-bind = SUPER, F, fullscreen,
+bind = $mainMod, RETURN, exec, alacritty
+bind = $mainMod, Q, killactive
+bind = $mainMod, E, exec, dolphin
+bind = $mainMod, V, togglefloating
+bind = $mainMod, SPACE, exec, rofi -show drun
  
-# Volume
 bind = , XF86AudioRaiseVolume, exec, pamixer -i 5
 bind = , XF86AudioLowerVolume, exec, pamixer -d 5
 bind = , XF86AudioMute, exec, pamixer -t
-CONFIG
 EOF
  
-echo "✅ Hoàn tất! Giờ bạn có Hyprland + blur + shadow + âm lượng hoạt động."
-echo "➡️ Exit rồi chạy: umount -R /mnt && reboot"
+chown -R hoang:hoang /home/hoang/.config
+ 
+echo "✅ DONE! Giờ bạn có thể thoát chroot và reboot!"
